ff24788a9f46908658ddab4095464b7eae3350b8
Maksimka1111 <92277262+Maksimka1111@users.noreply.github.com>
2022-10-04 02:01:19 +0300

    Add files via upload

---
+++ src/main/java/ru/minusd/terentiev/lab02/task03/pack3/Point.java
@@ -0,0 +1,21 @@
+package pack3;
+
+public class Point {
+    private double x = 0;
+    private double y = 0;
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double X) {
+        this.x = X;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double Y) {
+        this.y = Y;
+    }
+}
