d218794de08ca2685f1287bb1adf8c951bcce646
alex0d <alexey.d707@gmail.com>
2022-10-04 16:39:14 +0300

    denisov: update lab06\task10

--- src/main/java/ru/minusd/denisov/lab06/task10/TestShop.java
+++ src/main/java/ru/minusd/denisov/lab06/task10/TestShop.java
@@ -65,35 +65,3 @@ public class TestShop {
         }
     }
 }
-
-/*
-    Scanner scanner = new Scanner(System.in);
-
-        ArrayList<Computer> computers = new ArrayList<>();
-        System.out.print("Количество компьютеров: ");
-        int x = scanner.nextInt();
-        System.out.println("Введите названия компьютеров: ");
-        for (int i = 0; i < x; i++) {
-            computers.add(new Computer(scanner.next()));
-        }
-
-        Shop shop = new Shop(computers);
-
-        System.out.print("Добавьте еще один компьютер: ");
-        shop.addComputer(new Computer(scanner.next()));
-
-        System.out.print("Удалите один компьютер: ");
-        if (shop.deleteComputer(scanner.next())) {
-            System.out.println("Компьютер был успешно удален.");
-        } else {
-            System.out.println("Такого компьютера не было.");
-        }
-
-        System.out.print("Найти компьютер: ");
-        Computer requiredComputer = shop.findComputer(scanner.next());
-        if (requiredComputer != null) {
-            System.out.println(computers);
-        } else {
-            System.out.println("Компьютер не найден");
-        };
- */
